e0f300aa48a595a673a40ead0fed3d9a94de10b6
hangkuk7 <74293602+hangkuk7@users.noreply.github.com>
2021-09-09 16:55:57 +0900

    edited by hangkuk at 2021.09.09

--- CommandManager.py
+++ CommandManager.py
@@ -5,3 +5,6 @@ class CommandManager:
 
     def __init__(self):
         print('[CommandManager] init')
+        self._url = None
+        # default header setting
+        self.header = {'Accept': 'application/json', 'Content-Type': 'application/json'}
