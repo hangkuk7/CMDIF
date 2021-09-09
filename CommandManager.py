bb559b20210d2c64252d02adf8125ddad6774664
hangkuk7 <74293602+hangkuk7@users.noreply.github.com>
2021-09-09 17:31:50 +0900

    edited by hangkuk at 2021.09.09

--- CommandManager.py
+++ CommandManager.py
@@ -6,4 +6,21 @@ class CommandManager:
         print('[CommandManager] init')
         self._url = None
         # default header setting
-        self.header = {'Accept': 'application/json', 'Content-Type': 'application/json'}
+        self._header = {'Accept': 'application/json', 'Content-Type': 'application/json'}
+        self._rcmd_req_body = None
+        self._rcmd_cmd = None
+        self._rcmd_param = None
+        self._request_body = None
+
+    def set_cmd_url(self, url):
+        self._url = url
+
+    def set_cmd_header(self, headers):
+        self._header = headers
+
+    def set_rcmd_message(self, rcmd_command, rcmd_body):
+
+        # double Json Encoding
+        json_string = json.dumps(rcmd_body)
+        json_string = json.dumps(rcmd_body)
+
