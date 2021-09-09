8365bcd8d0be57905d3f575f14769caf74fd3015
hangkuk7 <74293602+hangkuk7@users.noreply.github.com>
2021-09-09 17:40:17 +0900

    edited by hangkuk at 2021.09.09

--- CommandManager.py
+++ CommandManager.py
@@ -18,9 +18,18 @@ class CommandManager:
     def set_cmd_header(self, headers):
         self._header = headers
 
-    def set_rcmd_message(self, rcmd_command, rcmd_body):
-
+    def set_rcmd_message(self, rcmd_prefix, rcmd_body):
         # double Json Encoding
         json_string = json.dumps(rcmd_body)
         json_string = json.dumps(rcmd_body)
+
+        self._rcmd_cmd = '%s %s;' % (rcmd_prefix, json_string)
+
+        return self._rcmd_cmd
+
+    def make_command(self, req_params):
+
+        json_req_params = json.dumps(req_params)
+
+        return json_req_params
 
